6534ec86806cc97271ecd753973696ddc3fe8a60
lihao <lihao3016@gmail.com>
2018-02-10 16:14:41 +0800

    sdf

--- imagemagick/draw.sh
+++ imagemagick/draw.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#draw a picture
-#draw a grid
 step=100;xMax=1080;yMax=1440;
+#draw a picture
 convert -size ${xMax}x${yMax} xc:lightblue demo.jpeg
+#draw a grid
 convert -fill black -draw "$(for n in $(seq 0 100 ${xMax});do echo line $n,0 $n,${yMax}; done)" demo.jpeg demo1.jpeg
 convert -fill black -draw "$(for n in $(seq 0 100 ${yMax});do echo line 0,$n ${xMax},$n; done)" demo1.jpeg demo1.jpeg
 eog demo1.jpeg
